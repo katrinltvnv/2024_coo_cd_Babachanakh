65541b2df734ce8bc7ff5fc5513a4425b6c24f1b
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-05-14 10:59:32 +0200

    Ajoute des fichiers XML

---
+++ TP magasin CD/test/TestChargeurMagasin.java
@@ -0,0 +1,15 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.io.FileNotFoundException;
+
+import XML.ChargeurMagasin;
+import donnees.Magasin;
+import org.junit.Test;
+
+public class TestChargeurMagasin {
+
+
+}
